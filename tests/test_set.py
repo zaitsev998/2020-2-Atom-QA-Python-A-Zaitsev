a14810dfbb5e6dff95b7fd5d9076957f66234a61
Artem <zaitsev998@mail.ru>
2020-09-27 16:17:45 +0300

    Tests on set

---
+++ tests/test_set.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+class TestSet:
+
+    def sets_definition(self):
+        self.first_set = {1, 3, 5, 7, 10}
+        self.second_set = {2, 3, 5, 8, 12}
+
+    def test_union(self):
+        self.sets_definition()
+        assert self.first_set.union(self.second_set) == self.first_set | self.second_set
+
+    def test_intersection(self):
+        self.sets_definition()
+        assert self.first_set.intersection(self.second_set) == self.first_set & self.second_set
+
+    def test_difference(self):
+        self.sets_definition()
+        assert self.first_set.difference(self.second_set) == self.first_set - self.second_set
+
+    def test_issubset(self):
+        self.sets_definition()
+        assert self.first_set.issubset(self.second_set) == (self.first_set <= self.second_set)
+
+    @pytest.mark.parametrize(('init_set', 'item', 'result'),
+                             (({'first', 'second'}, 'second', {'first'}),
+                              ({'first', 'second'}, 'third', {'first', 'second'})))
+    def test_discard(self, init_set, item, result):
+        init_set.discard(item)
+        assert init_set == result
